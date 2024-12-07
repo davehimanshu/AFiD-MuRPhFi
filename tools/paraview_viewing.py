88473636ec33d66abeac9cc867d85a4f495a346c
Himanshu Dave <himanshu106@gmail.com>
2024-12-07 14:31:40 +0100

    updated the script to outout xmf files so that all the variables are scaled to the same spatial resolution

--- tools/paraview_viewing.py
+++ tools/paraview_viewing.py
@@ -1,9 +1,18 @@
 import afidtools as afid
 import numpy as np
 folder = "/scratch/seismo/dave/melting_Ross/0.3"
-vars = ["temp"]
+vars = ["temp","phi"]
 for var in vars:
-    afid.interpolate_field_to_uniform(folder, var, scale=4)
-    print("Interpolated "+var+" to uniform")
-    afid.generate_uniform_xmf(folder, var, scale=4)
-    print("Created xmf for "+var)
+    if var == "phi":
+        afid.interpolate_field_to_uniform(folder, var, scale=2)
+        print("Interpolated "+var+" to uniform")
+        afid.generate_uniform_xmf(folder, var, scale=2)
+        print("Created xmf for "+var)
+    else:
+        afid.interpolate_field_to_uniform(folder, var, scale=1)
+        print("Interpolated "+var+" to uniform")
+        afid.generate_uniform_xmf(folder, var, scale=1)
+        print("Created xmf for "+var)
+
+
+    
